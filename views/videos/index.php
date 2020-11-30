8360c2f8a17f476118a80d8f71586c02c59b88ed
Thomas Hackl <thomas.hackl@uni-passau.de>
2020-11-30 12:02:15 +0100

    allow single and double quotes in video filenames

--- views/videos/index.php
+++ views/videos/index.php
@@ -1,5 +1,5 @@
 <div id="videos">
-    <video-list :dates='<?php echo studip_json_encode($assigned_dates) ?>'
+    <video-list :dates='<?php echo studip_json_encode($assigned_dates, JSON_HEX_QUOT + JSON_HEX_APOS) ?>'
                 get-video-src-url="<?php echo $controller->link_for('videos/get_src') ?>"
         <?php if ($permission) : ?>
                 edit-url="<?php echo $controller->link_for('videos/edit') ?>"
